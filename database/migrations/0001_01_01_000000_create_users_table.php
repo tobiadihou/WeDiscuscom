d1113b768893a92ce22ed9e04a55f3b4c2c977be
tobiadihou <adihoutobi@gmail.com>
2024-08-02 12:29:14 +0200

    user migration creer

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('avatar')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_admin')->default(false);
+            $table->timestamp('blocked_at')->nullable();
+            $table-
             $table->rememberToken();
             $table->timestamps();
         });
